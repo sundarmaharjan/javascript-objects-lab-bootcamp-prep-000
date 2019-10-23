779e6b7cdcbb53169c06788e9ee6b5cce532048f
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-23 11:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deleteFromObjectByKey(object, key){
   
 }
 
-function destructivelydeleteFromObjectByKey(object,key) {
+function destructivelyDeleteFromObjectByKey(object,key) {
   delete object[key];
   return object ;
 }
