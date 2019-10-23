13141ba5193021d7a16cf4d0cf5ad1d2c13865c6
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-23 11:42:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ function updateObjectWithKeyAndValue (object, key, value){
 }
 
 
+function  destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+
+function deleteFromObjectByKey(object, value){
+  var newObj = Object.assign({}, object);
+  delete newObj [key];
+  return newObj;
+  
+}
+
+function destructivelydeleteFromObjectByKey(object,key) {
+    return object.key = "undefined";
+}
+ obj = destructivelydeleteFromObjectByKey(obj, "prop")}
