bf018e9256dd5b25b3ba0edaded951a03e501030
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-23 11:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue (object, key, value){
-  return Object.assign ({},object, {[key]: value});
+  return Object.assign ({},object, {[key]: value})
 }
 
 
